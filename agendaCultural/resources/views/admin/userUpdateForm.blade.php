077820f6676a92eebc3be24004f7b9f7e3edfe2c
daniMB94 <dani_aliast@hotmail.com>
2024-03-28 19:34:58 +0100

    nada nuevo

--- agendaCultural/resources/views/admin/userUpdateForm.blade.php
+++ agendaCultural/resources/views/admin/userUpdateForm.blade.php
@@ -165,82 +165,113 @@
         <!-- end navbar -->
 
         <!-- Content -->
+        <section class="container px-4 mx-auto">
+            <div class="sm:flex sm:items-center sm:justify-between">
+                <div>
+                    <div class="flex items-center gap-x-3">
+                        <h2 class="text-lg font-medium text-gray-800 dark:text-white">Modificar usuario</h2>
 
-        <!-- component -->
-        <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
-            <div class="-mx-3 md:flex mb-6">
-                <div class="md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                        for="grid-first-name">
-                        First Name
-                    </label>
-                    <input
-                        class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
-                        id="grid-first-name" type="text" placeholder="Jane">
-                    <p class="text-red text-xs italic">Please fill out this field.</p>
+                    </div>
+
                 </div>
-                <div class="md:w-1/2 px-3">
-                    <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                        for="grid-last-name">
-                        Last Name
-                    </label>
-                    <input
-                        class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-last-name" type="text" placeholder="Doe">
+
+                <div class="flex items-center mt-4 gap-x-3">
+
+
+                    <button
+                        class="flex items-center justify-center w-1/2 px-5 py-2 text-sm tracking-wide text-white transition-colors duration-200 bg-red-500 rounded-lg shrink-0 sm:w-auto gap-x-2 hover:bg-red-600 dark:hover:bg-red-500 dark:bg-red-600">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24"
+                            style="fill: rgba(255, 255, 255, 1);transform: ;msFilter:;">
+                            <path
+                                d="M12.48 3 7.73 7.75 3 12.59a2 2 0 0 0 0 2.82l4.3 4.3A1 1 0 0 0 8 20h12v-2h-7l7.22-7.22a2 2 0 0 0 0-2.83L15.31 3a2 2 0 0 0-2.83 0zM8.41 18l-4-4 4.75-4.84.74-.75 4.95 4.95-4.56 4.56-.07.08z">
+                            </path>
+                        </svg>
+
+
+                        <span>Eliminar usuario</span>
+                    </button>
                 </div>
             </div>
-            <div class="-mx-3 md:flex mb-6">
-                <div class="md:w-full px-3">
-                    <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                        for="grid-password">
-                        Password
-                    </label>
-                    <input
-                        class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3"
-                        id="grid-password" type="password" placeholder="******************">
-                    <p class="text-grey-dark text-xs italic">Make it as long and as crazy as you'd like</p>
-                </div>
-            </div>
-            <div class="-mx-3 md:flex mb-2">
-                <div class="md:w-1/2 px-3 mb-6 md:mb-0">
-                    <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                        for="grid-city">
-                        City
-                    </label>
-                    <input
-                        class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-city" type="text" placeholder="Albuquerque">
-                </div>
-                <div class="md:w-1/2 px-3">
-                    <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
-                        for="grid-state">
-                        State
-                    </label>
-                    <div class="relative">
-                        <select
-                            class="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded"
-                            id="grid-state">
-                            <option>New Mexico</option>
-                            <option>Missouri</option>
-                            <option>Texas</option>
-                        </select>
-                        <div class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
-                            <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
-                                <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
-                            </svg>
-                        </div>
+
+            <!-- component -->
+            <div class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4 flex flex-col my-2">
+                <div class="-mx-3 md:flex mb-6">
+                    <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                            for="grid-first-name">
+                            First Name
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-red rounded py-3 px-4 mb-3"
+                            id="grid-first-name" type="text" placeholder="Jane">
+                        <p class="text-red text-xs italic">Please fill out this field.</p>
+                    </div>
+                    <div class="md:w-1/2 px-3">
+                        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                            for="grid-last-name">
+                            Last Name
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
+                            id="grid-last-name" type="text" placeholder="Doe">
                     </div>
                 </div>
-                <div class="md:w-1/2 px-3">
-                    <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2" for="grid-zip">
-                        Zip
-                    </label>
-                    <input
-                        class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
-                        id="grid-zip" type="text" placeholder="90210">
+                <div class="-mx-3 md:flex mb-6">
+                    <div class="md:w-full px-3">
+                        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                            for="grid-password">
+                            Password
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4 mb-3"
+                            id="grid-password" type="password" placeholder="******************">
+                        <p class="text-grey-dark text-xs italic">Make it as long and as crazy as you'd like</p>
+                    </div>
+                </div>
+                <div class="-mx-3 md:flex mb-2">
+                    <div class="md:w-1/2 px-3 mb-6 md:mb-0">
+                        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                            for="grid-city">
+                            City
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
+                            id="grid-city" type="text" placeholder="Albuquerque">
+                    </div>
+                    <div class="md:w-1/2 px-3">
+                        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                            for="grid-state">
+                            State
+                        </label>
+                        <div class="relative">
+                            <select
+                                class="block appearance-none w-full bg-grey-lighter border border-grey-lighter text-grey-darker py-3 px-4 pr-8 rounded"
+                                id="grid-state">
+                                <option>New Mexico</option>
+                                <option>Missouri</option>
+                                <option>Texas</option>
+                            </select>
+                            <div
+                                class="pointer-events-none absolute pin-y pin-r flex items-center px-2 text-grey-darker">
+                                <svg class="h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
+                                    <path
+                                        d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z" />
+                                </svg>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="md:w-1/2 px-3">
+                        <label class="block uppercase tracking-wide text-grey-darker text-xs font-bold mb-2"
+                            for="grid-zip">
+                            Zip
+                        </label>
+                        <input
+                            class="appearance-none block w-full bg-grey-lighter text-grey-darker border border-grey-lighter rounded py-3 px-4"
+                            id="grid-zip" type="text" placeholder="90210">
+                    </div>
                 </div>
             </div>
-        </div>
+        </section>
 
         <!-- End Content -->
     </main>

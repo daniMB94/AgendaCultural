67e692af91dcb87049ff66355f4783dfd5b3d752
daniMB94 <dani_aliast@hotmail.com>
2024-04-15 20:41:59 +0200

    Intentando dar diseño

--- agendaCultural/resources/views/asistente/eventos.blade.php
+++ agendaCultural/resources/views/asistente/eventos.blade.php
@@ -44,7 +44,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg bg-opacity-80">
                 <div class="grid grid-cols-4 gap-4 p-6 text-gray-900 dark:text-gray-100">
                     @foreach ($eventos as $evento)
 
--- agendaCultural/resources/views/asistente/experiencias.blade.php
+++ agendaCultural/resources/views/asistente/experiencias.blade.php
@@ -5,61 +5,82 @@
         </h2>
     </x-slot>
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-
-                    <!-------------------------------------------------->
-                    <table class="border-collapse border border-slate-400 w-full divide-y-8">
+    <div class="p-8 rounded-lg">
 
 
-                        @foreach($experiencias as $experiencia)
+        <!-------------------------------------------------->
+        <!-- component -->
+        <section>
+            <div class="bg-black text-white py-8">
+                <div class="container mx-auto flex flex-col items-start md:flex-row my-12 md:my-24">
+                    <div class="flex flex-col w-full sticky md:top-36 lg:w-1/3 mt-2 md:mt-12 px-8">
+                        <p class="ml-2 text-yellow-300 uppercase tracking-loose">Working Process</p>
+                        <p class="text-3xl md:text-4xl leading-normal md:leading-relaxed mb-2">Working
+                            Process of Fest</p>
+                        <p class="text-sm md:text-base text-gray-50 mb-4">
+                            Here’s your guide to the tech fest 2021 process. Go through all the steps to
+                            know the exact process of the
+                            fest.
+                        </p>
+                        <a href="#" class="bg-transparent mr-auto hover:bg-yellow-300 text-yellow-300 hover:text-white rounded shadow hover:shadow-lg py-2 px-4 border border-yellow-300 hover:border-transparent">
+                            Explore Now</a>
+                    </div>
+                    <div class="ml-0 md:ml-12 lg:w-2/3 sticky">
+                        <div class="container mx-auto w-full h-full">
+                            <div class="relative wrap overflow-hidden p-10 h-full">
+                                <div class="border-2-2 border-yellow-555 absolute h-full border" style="right: 50%; border: 2px solid #FFC100; border-radius: 1%;"></div>
+                                <div class="border-2-2 border-yellow-555 absolute h-full border" style="left: 50%; border: 2px solid #FFC100; border-radius: 1%;"></div>
+                                @php
+                                $contador = 0;
+
+                                @endphp
+                                @foreach($experiencias as $experiencia)
+                                @php
+                                $contador++;
+                                @endphp
+                                @if($contador%2 == 0)
+                                <div class="mb-8 flex justify-between flex-row-reverse items-center w-full left-timeline">
+                                    <div class="order-1 w-5/12"></div>
+                                    <div class="order-1 w-5/12 px-1 py-4 text-right">
+                                        <p class="mb-3 text-base text-green-300">
+                                            {{$experiencia->fechaInicio}}
+                                        </p>
+                                        <h4 class="mb-3 font-bold text-lg md:text-2xl">
+                                            {{$experiencia->nombre}}
+                                        </h4>
+                                        <p class="text-sm md:text-base leading-snug text-black-50 text-opacity-100">
+                                            {{$experiencia->descripcionLarga}}
+                                        </p>
+                                    </div>
+                                </div>
+                                @else
+                                <div class="mb-8 flex justify-between items-center w-full right-timeline">
+                                    <div class="order-1 w-5/12"></div>
+                                    <div class="order-1  w-5/12 px-1 py-4 text-left">
+                                        <p class="mb-3 text-base text-green-300">
+                                            {{$experiencia->fechaInicio}}
+                                        </p>
+                                        <h4 class="mb-3 font-bold text-lg md:text-2xl">
+                                            {{$experiencia->nombre}}
+                                        </h4>
+                                        <p class="text-sm md:text-base leading-snug text-black-50 text-opacity-100">
+                                            {{$experiencia->descripcionLarga}}
+                                        </p>
+                                    </div>
+                                </div>
+                                @endif
 
 
-
-                        <tbody>
-                            <tr>
-                                <td class="text-center">
-                                    <h1 class="overline decoration-indigo-500 m-3 text-2xl">
-                                        {{$experiencia->empresa->nombre}}
-                                    </h1>
-                                </td>
-                                <td>
-                                    <p>
-                                        {{$experiencia->empresa->telefono}}
-                                    </p>
-                                    <p>{{$experiencia->empresa->email}}</p>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td
-                                    class="border border-slate-300 bg-green-400 text-3xl font-extrabold text-white text-center">
-                                    <p>Fecha de inicio: {{$experiencia->fechaInicio}}</p>
-                                    <br>
-                                    <p>Fecha de Fin: {{$experiencia->fechaFin}}</p>
-                                </td>
-                                <td class="border border-slate-300">
-                                    <img class="w-full h-48 object-cover"
-                                        src="{{ asset('images/' . $experiencia->imagen) }}"
-                                        alt="Descripción de la imagen">
-                                </td>
-                                <td class="border border-slate-300">{{$experiencia->descripcionCorta}}</td>
-                                <td class="border border-slate-300"><a href="{{$experiencia->empresa->web}}"
-                                        class="text-indigo-700 underline">Web de
-                                        inscripcion</a></td>
-                            </tr>
-                        </tbody>
-
-
-                        @endforeach
-                    </table>
-
-                    <!-------------------------------------------------->
-
+                                @endforeach
+                            </div>
+                            <img class="mx-auto -mt-36 md:-mt-36" src="https://user-images.githubusercontent.com/54521023/116968861-ef21a000-acd2-11eb-95ac-a34b5b490265.png" />
+                        </div>
+                    </div>
                 </div>
-
             </div>
-        </div>
+        </section>
+        <!-------------------------------------------------->
+
+
     </div>
 </x-app-layout>
